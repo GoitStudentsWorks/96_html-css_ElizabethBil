e3457b0b82d2ca719f4483889b75820174a64621
TetiZ <tommytaba99@gmail.com>
2023-10-04 10:40:22 +0200

    page title, modal.js update

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,10 +3,14 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    closeMenuLink: document.querySelectorAll('.header-mobile-nav-link'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeMenuLink.forEach(item =>
+    item.addEventListener('click', toggleModal)
+  );
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
